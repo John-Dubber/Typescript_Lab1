d420eeecc08c0bf264d126b8acddf3b3efa084f6
John <jonnydubber@gmail.com>
2020-01-20 17:12:08 +0000

    added 2 new files

---
+++ types.js
@@ -0,0 +1,8 @@
+var s = "Hello World";
+console.log(s);
+var n = 99; //numbers can be floating point or integer
+console.log("n is now" + n);
+n = 99.9;
+console.log("n is now:" + n);
+var b = true;
+console.log(b);
---
+++ types.ts
@@ -0,0 +1,8 @@
+let s: string = "Hello World";
+console.log(s);
+let n: number = 99;//numbers can be floating point or integer
+console.log("n is now"+ n);
+n = 99.9;
+console.log("n is now:"+ n);
+let b: boolean = true;
+console.log(b);
